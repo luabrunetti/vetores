9d413623138af6a9ccf67fe7d55d4cdceb54b8f8
Luana Brunetti de Oliveira <162700949+luabrunetti@users.noreply.github.com>
2025-06-30 11:37:08 -0300

    Create exercicio1.js

---
+++ exercicio1.js
@@ -0,0 +1,13 @@
+// Exercício 1
+
+const animais = ["Cachorro", "Gato", "Leão", "Tigre", "Elefante"];
+
+function contarElementos(array) {
+    let contador = 0;
+    for (let i = 0; i < array.length; i++) {
+        contador++;
+    }
+    return contador;
+}
+
+console.log(contarElementos(animais));
